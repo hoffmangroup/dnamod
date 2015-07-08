bb6381360f91017ea3fb3aa296cb3220d95e7814
Jai Sood <ankur.jai.sood@gmail.com>
2015-07-08 12:47:52 -0400

    Add necessary files to hg ignore

--- jinja2test.py
+++
@@ -1,5 +0,0 @@
-from Jinja2 import Environment, PackageLoader
-
-env = Environment(loader=Packageloader('/users/asood/My Documents', 'Templates'))
-
-template = env.get_template('test_template.html')
