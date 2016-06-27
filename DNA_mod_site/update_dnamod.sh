42906580b5f3da308244e1721178cf5d699a3b4c
Coby Viner <coby.viner@iscb.org>
2016-06-27 18:10:32 -0400

    Revised update script to delete the existing database first.

--- DNA_mod_site/update_dnamod.sh
+++ DNA_mod_site/update_dnamod.sh
@@ -5,8 +5,14 @@ SOURCE_DIR="$(dirname $(readlink -f $0))" # From: https://gist.github.com/tvlooy
 
 # Updates DNAmod database and generates static site
 echo "Begining to update DNAmod and create static site"
+
+# remove any existing database to fully re-create it
+rm -f $SOURCE_DIR/../DNA_mod_database.db
+
 $SOURCE_DIR/../populate_database_sql.py
 echo "Database updated"
+
 $SOURCE_DIR/create_mod_staticsite_sql.py
+
 echo "Static site created"
 
