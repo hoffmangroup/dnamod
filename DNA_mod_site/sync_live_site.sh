1a1dd45724c1a6f4b58e07caf6cfc7672e7fc76c
Coby Viner <coby.viner@iscb.org>
2016-06-27 17:12:48 -0400

    Made site sync script runnable by other Hoffman Lab members.

--- DNA_mod_site/sync_live_site.sh
+++ DNA_mod_site/sync_live_site.sh
@@ -3,12 +3,15 @@ set -o nounset -o pipefail -o errexit
 
 # Copy contents of ./static/* into directory for live site
 
+SOURCE_DIR="$(dirname $(readlink -f $0))" # From: https://gist.github.com/tvlooy/cbfbdb111a4ebad8b93e
+
 # Testing mode: If this is set to true the site will be copied into a test folder in ~/DNA_Base_Database/DNA_mod_site
 TESTING_MODE=false
 
-MAIN_SITE_DIR='/mnt/work1/users/home2/asood/DNA_Base_Database/DNA_mod_site/static'
+MAIN_SITE_DIR="$SOURCE_DIR/static"
+
 # Path variables for testing mode and real mode
-TEST_PATH='/mnt/work1/users/home2/asood/DNA_Base_Database/DNA_mod_site/sync_test'
+TEST_PATH="$SOURCE_DIR/sync_test"
 REAL_PATH='/mnt/work1/users/hoffmangroup/www/proj/dnamod'
 
 if [[ "$TESTING_MODE" == true ]]; then
