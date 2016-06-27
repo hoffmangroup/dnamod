e3176a74a4feea2cce1a3b2e0b29c6fbee3139f3
Jai Sood <ankur.jai.sood@gmail.com>
2016-06-27 13:50:22 -0400

    Added expanded alphabet file and code to add expanded alphabet to site

--- DNA_mod_site/update_dnamod.sh
+++ DNA_mod_site/update_dnamod.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -o nounset -o pipefail -o errexit
 
+SOURCE_DIR="$(dirname $(readlink -f $0))" # From: https://gist.github.com/tvlooy/cbfbdb111a4ebad8b93e
+
 # Updates DNAmod database and generates static site
 echo "Begining to update DNAmod and create static site"
-cd /mnt/work1/users/home2/asood/DNA_Base_Database
-python populate_database_sql.py
+$SOURCE_DIR/../populate_database_sql.py
 echo "Database updated"
-cd /mnt/work1/users/home2/asood/DNA_Base_Database/DNA_mod_site
-python create_mod_staticsite_sql.py
+$SOURCE_DIR/create_mod_staticsite_sql.py
 echo "Static site created"
 
