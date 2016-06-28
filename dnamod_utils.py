11e417d0e31c9c8bc58c9cef7f24dd11e3c31eec
Coby Viner <coby.viner@iscb.org>
2016-06-28 12:34:40 -0400

    Fixed flake8 errors.

--- dnamod_utils.py
+++ dnamod_utils.py
@@ -7,9 +7,11 @@ import os
 
 FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
+
 def get_list(listname):
     returnlist = []
-    with open(FILE_PATH + '/DNA_mod_site/static/whitelist/'+listname, 'r') as targetlist:
+    with open(FILE_PATH + '/DNA_mod_site/static/whitelist/'+listname,
+              'r') as targetlist:
         reader = csv.reader(targetlist, dialect='excel-tab')
         for line in reader:
             if line == []:
