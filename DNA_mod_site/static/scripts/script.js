6190842578fb6685940e74d9d7242356c712be30
Jai Sood <ankur.jai.sood@gmail.com>
2016-08-11 07:27:38 -0400

    Links working with colour, removed list bullets

--- DNA_mod_site/static/scripts/script.js
+++ DNA_mod_site/static/scripts/script.js
@@ -79,7 +79,11 @@ $(document).ready(function() {
             resultdiv.empty();
             for (var j in result) {
                 console.log(result[j])
-                resultdiv.append('<li> <a href=' + result[j].ref + ' >' + store[result[j].ref]['Common Name'] + '</a> </li>');
+                if (store[result[j].ref]['Verified'] === 1) {
+                    resultdiv.append('<li> <a href=' + result[j].ref + ' style="color:green">' + store[result[j].ref]['Common Name'] + '</a> </li>');
+                } else {
+                    resultdiv.append('<li> <a href=' + result[j].ref + ' style="color:red">' + store[result[j].ref]['Common Name'] + '</a> </li>');                
+                }
             }
         }
     });
