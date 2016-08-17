93d0a94eaee7e8ddd5786ce20d5cb53164bac9df
Coby Viner <coby.viner@iscb.org>
2016-08-17 12:20:36 -0400

    Fixed errors in the image directory path, in which the directory's location could depend upon from where the script was invoked. Also refactored code to resolve issues introduced in 1fd53d1.

--- DNA_mod_site/create_mod_staticsite_sql.py
+++ DNA_mod_site/create_mod_staticsite_sql.py
@@ -64,32 +64,32 @@ def is_list(object):
 
 def render_image(smiles, name):
     """Creates images of chemical structures from their SMILES, with PyBel.
-       The SVG image is saved. The path of the image is returned, if it exists.
-       Otherwise, None is returned.
+       The SVG image is saved. The relative path of the image is returned,
+       if it exists. Otherwise, None is returned.
     """
 
-    # get the relative path, from the site HTML directory
-    img_dir = os.path.relpath(dnamod_utils.get_constant('site_image_dir'),
-                              dnamod_utils.get_constant('site_dir'))
+    image_dir = dnamod_utils.get_constant('site_image_dir')
 
-    if not os.path.exists(img_dir):
-        os.makedirs(img_dir)
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
     if not smiles:
         return
 
-    image_path = os.path.relpath(dnamod_utils.get_constant('site_image_dir'),
-                              HTML_FILES_DIR)
-    image_path_ext = os.path.join(image_path, "{}.{}".format(name, IMAGE_FORMAT))
+    image_path = os.path.join(image_dir,
+                              "{}.{}".format(name, IMAGE_FORMAT))
 
     mol = pybel.readstring('smi', smiles)
-    mol.write(IMAGE_FORMAT, image_path_ext, overwrite=True)
+    mol.write(IMAGE_FORMAT, image_path, overwrite=True)
 
-    if not os.path.isfile(image_path_ext):
+    if not os.path.isfile(image_path):
         print("Warning: failed to render image"
               "for {}".format(name), file=sys.stderr)
         image_path = None
 
-    return image_path_ext
+    # return the relative path, from the site HTML directory
+    image_relpath = os.path.relpath(image_path, HTML_FILES_DIR)
+
+    return image_relpath
 
 
 def get_citations(lookup_key, cursor):
